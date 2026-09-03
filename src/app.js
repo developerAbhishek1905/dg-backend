d58fd1501641b56e0d9d223c3f936bb0f91f2d74
developerAbhishek1905 <developer.abhishek1905@gmail.com>
2026-09-03 18:38:47 +0530

    cors

--- src/app.js
+++ src/app.js
@@ -16,7 +16,20 @@ import areaRoutes from "./modules/address/routes/area.routes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "http://localhost:5175",
+      "http://localhost:5176",
+      "https://dg-iota-tawny.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
